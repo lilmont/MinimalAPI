2656b094e9aae011c2e754209c30fe48065e5bc3
Niloo Mont <niloo.mon@gmail.com>
2024-03-14 16:40:18 +0330

    Init: implement create

--- MinimalAPI/MinimalAPI/Program.cs
+++ MinimalAPI/MinimalAPI/Program.cs
@@ -1,3 +1,5 @@
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,15 +11,19 @@ builder.Services.AddDbContext<TodoDbContext>(config =>
 
 var app = builder.Build();
 
+var todoEndpoints = new Endpoints<Todo>(app.MapGroup("/todoItems"));
+
+todoEndpoints.WithCreate(configure => configure
+.RegisterValidator<TodoCreateValidation>()
+.RegisterResponseModel<TodoCreateResponse>()
+.RegisterResponseModel<TodoCreateResponse>()
+);
+
 app.Run();
 
-class TodoDbContext : DbContext
+#region Database
+class TodoDbContext(DbContextOptions<TodoDbContext> options) : DbContext(options)
 {
-    public TodoDbContext(DbContextOptions<TodoDbContext> options)
-        : base(options)
-    {
-
-    }
     public DbSet<Todo> Todos { get; set; }
 }
 
@@ -27,3 +33,63 @@ class Todo
     public required string Title { get; set; }
     public bool IsCompleted { get; set; }
 }
+#endregion
+
+class Endpoints<TEntity>(RouteGroupBuilder mapGroup)
+    where TEntity : class
+{
+    public Endpoints<TEntity> WithCreate(Action<CreateEndpointConfiguration<TEntity>> configure)
+    {
+        mapGroup.MapPost("/", ([FromBody] TEntity entity, TodoDbContext dbContext) =>
+        {           
+            dbContext.Set<TEntity>().Add(entity);
+            dbContext.SaveChanges();
+            return Results.Ok("Created.");
+        });
+        return this;
+    }
+}
+
+#region TodoCreate
+class CreateEndpointConfiguration<TEntity>
+{
+    public CreateEndpointConfiguration<TEntity> RegisterValidator<TValidator>()
+        where TValidator : class
+    {
+        return this;
+    }
+
+    public CreateEndpointConfiguration<TEntity> RegisterRequestModel<TRequestModel>()
+    {
+        return this;
+    }
+
+    public CreateEndpointConfiguration<TEntity> RegisterResponseModel<TResponseModel>()
+    {
+        return this;
+    }
+}
+
+class TodoCreateValidation : AbstractValidator<Todo>
+{
+    public TodoCreateValidation()
+    {
+        RuleFor(p => p.Title)
+            .NotEmpty().WithMessage("Title cannot be empty!")
+            .NotNull().WithMessage("Please include title!");
+    }
+}
+
+class TodoCreateRequest
+{
+    public string Title { get; set; }
+    public bool IsCompleted { get; set; }
+}
+
+class TodoCreateResponse
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool IsCompleted { get; set; }
+}
+#endregion
