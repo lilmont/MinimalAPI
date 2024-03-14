1e270a204ff83402fea774a8359e9cb747c5b1b3
Niloo Mont <niloo.mon@gmail.com>
2024-03-14 16:54:07 +0330

    Init: implement update

--- MinimalAPI/MinimalAPI/Program.cs
+++ MinimalAPI/MinimalAPI/Program.cs
@@ -13,11 +13,12 @@ var app = builder.Build();
 
 var todoEndpoints = new Endpoints<Todo>(app.MapGroup("/todoItems"));
 
-todoEndpoints.WithCreate(configure => configure
-.RegisterValidator<TodoCreateValidation>()
-.RegisterResponseModel<TodoCreateResponse>()
-.RegisterResponseModel<TodoCreateResponse>()
-);
+todoEndpoints
+    .WithCreate(configure => configure
+    .RegisterValidator<TodoCreateValidation>()
+    .RegisterResponseModel<TodoCreateResponse>()
+    .RegisterResponseModel<TodoCreateResponse>())
+    ;
 
 app.Run();
 
@@ -43,8 +44,40 @@ class Endpoints<TEntity>(RouteGroupBuilder mapGroup)
         mapGroup.MapPost("/", ([FromBody] TEntity entity, TodoDbContext dbContext) =>
         {           
             dbContext.Set<TEntity>().Add(entity);
-            dbContext.SaveChanges();
-            return Results.Ok("Created.");
+
+            try
+            {
+                dbContext.SaveChanges();
+                return Results.Ok("Created!");
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem("Something went wrong: " + ex.Message);
+            }
+        });
+        return this;
+    }
+
+    public Endpoints<TEntity> WithUpdate(Action<CreateEndpointConfiguration<TEntity>> configure)
+    {
+        mapGroup.MapPut("/{id}", ([FromBody] TEntity entity,[FromRoute] int id, TodoDbContext dbContext) =>
+        {
+            var currentEntity = dbContext.Set<TEntity>().Find(id);
+            if (currentEntity is null)
+            {
+                return Results.NotFound();
+            }
+            dbContext.Entry(currentEntity).CurrentValues.SetValues(entity);
+
+            try
+            {
+                dbContext.SaveChanges();
+                return Results.Ok("Updated!");
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem("Something went wrong: " + ex.Message);
+            }
         });
         return this;
     }
@@ -87,6 +120,49 @@ class TodoCreateRequest
 }
 
 class TodoCreateResponse
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool IsCompleted { get; set; }
+}
+#endregion
+
+#region TodoUpdate
+class UpdateEndpointConfiguration<TEntity>
+{
+    public UpdateEndpointConfiguration<TEntity> RegisterValidator<TValidator>()
+        where TValidator : class
+    {
+        return this;
+    }
+
+    public UpdateEndpointConfiguration<TEntity> RegisterRequestModel<TRequestModel>()
+    {
+        return this;
+    }
+
+    public UpdateEndpointConfiguration<TEntity> RegisterResponseModel<TResponseModel>()
+    {
+        return this;
+    }
+}
+class TodoUpdateValidation : AbstractValidator<Todo>
+{
+    public TodoUpdateValidation()
+    {
+        RuleFor(p => p.Title)
+            .NotEmpty().WithMessage("Title cannot be empty!")
+            .NotNull().WithMessage("Please include title!");
+    }
+}
+
+class TodoUpdateRequest
+{
+    public string Title { get; set; }
+    public bool IsCompleted { get; set; }
+}
+
+class TodoUpdateResponse
 {
     public int Id { get; set; }
     public string Title { get; set; }
