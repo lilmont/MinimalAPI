5c53942e6c3a50f74e055d161f4ad3ebb1e9af14
Niloo Mont <niloo.mon@gmail.com>
2024-03-14 14:19:29 +0330

    Init: setup database

--- MinimalAPI/MinimalAPI/Program.cs
+++ MinimalAPI/MinimalAPI/Program.cs
@@ -1,6 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TodoDbContext>(config =>
+{
+    config.UseInMemoryDatabase("MinimalApiDb");
+});
 
 var app = builder.Build();
 
 app.Run();
 
+class TodoDbContext : DbContext
+{
+    public TodoDbContext(DbContextOptions<TodoDbContext> options)
+        : base(options)
+    {
+
+    }
+    public DbSet<Todo> Todos { get; set; }
+}
+
+class Todo
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public bool IsCompleted { get; set; }
+}
